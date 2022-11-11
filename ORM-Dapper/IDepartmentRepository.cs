14adb0860242fb420638d4bff9267f71464d6ace
Peter luque <Peterluque21@gmail.com>
2022-11-11 16:28:52 -0500

    commit

---
+++ ORM-Dapper/IDepartmentRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+    internal interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAllDepartments(); //Stubbed out method
+
+        void InsertDepartment(string newDepartmentName);
+    }
+}
--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace ORM_Dapper
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ORM_Dapper
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+            IDbConnection conn = new MySqlConnection(connString);
+
+            var repo = new DapperDepartmentRepository(conn);
+
+            Console.WriteLine("Type a new department name:");
+
+            var departmentName = Console.ReadLine();
+
+            repo.InsertDepartment(departmentName);
+
+            var departments = repo.GetAllDepartments();
+
+            foreach (var department in departments)
+            {
+                Console.WriteLine(department.Name);
+            }
         }
     }
 }
